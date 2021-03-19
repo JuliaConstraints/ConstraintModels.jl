d9e9f8850d394cb7331e17a7b277f24abe3db476
azzaare <jf@baffier.fr>
2021-03-19 16:26:02 +0900

    Basic split from LocalSearchSolvers.jl

--- src/ConstraintModels.jl
+++ src/ConstraintModels.jl
@@ -1,5 +1,25 @@
 module ConstraintModels
 
-# Write your package code here.
+using CBLS
+using Constraints
+using Dictionaries
+using JuMP
+using LocalSearchSolvers
+
+const LS = LocalSearchSolvers
+
+import LocalSearchSolvers: Options
+
+export mincut
+export golomb
+export magic_square
+export n_queens
+export sudoku
+
+include("cut.jl")
+include("golomb.jl")
+include("magic_square.jl")
+include("n_queens.jl")
+include("sudoku.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
+using CBLS
 using ConstraintModels
+using Dictionaries
+using JuMP
+using LocalSearchSolvers
+using MathOptInterface
 using Test
 
 @testset "ConstraintModels.jl" begin
-    # Write your tests here.
+    include("raw_solver.jl")
+    include("MOI_wrapper.jl")
+    include("JuMP.jl")
 end
