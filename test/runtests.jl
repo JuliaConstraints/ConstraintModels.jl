bbf0574c485a30d4023cfab471a546bc40f8ef3d
Azzaare <jf@baffier.fr>
2021-03-22 15:37:18 +0900

    Add instances.jl file in test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ using MathOptInterface
 using Test
 
 @testset "ConstraintModels.jl" begin
+    include("instances.jl")
     include("raw_solver.jl")
     include("MOI_wrapper.jl")
     include("JuMP.jl")
