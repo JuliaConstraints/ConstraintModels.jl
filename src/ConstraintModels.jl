750c277617ded157be80650a6d86278a48984ed2
Azzaare <jf@baffier.fr>
2021-03-22 22:26:44 +0900

    Add QAP as an available model

--- src/ConstraintModels.jl
+++ src/ConstraintModels.jl
@@ -10,12 +10,14 @@ const LS = LocalSearchSolvers
 
 import LocalSearchSolvers: Options
 
+export qap
 export mincut
 export golomb
 export magic_square
 export n_queens
 export sudoku
 
+include("assignment.jl")
 include("cut.jl")
 include("golomb.jl")
 include("magic_square.jl")
---
+++ src/assignment.jl
@@ -0,0 +1,13 @@
+function qap(n, W, D, ::Val{:JuMP})
+    model = JuMP.Model(CBLS.Optimizer)
+
+    @variable(model, X[1:n], DiscreteSet(1:n))
+    @constraint(model, X in AllDifferent())
+
+    Σwd = p -> sum(sum(W[p[i], p[j]] * D[i, j] for j in 1:n) for i in 1:n)
+    @objective(model, Min, ScalarFunction(Σwd))
+
+    return model, X
+end
+
+qap(n, weigths, distances; modeler = :JuMP) = qap(n, weigths, distances, Val(modeler))
