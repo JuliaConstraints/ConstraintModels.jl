0e6af3711895635796d3f1a4eba59b8529e5d1f0
azzaare <jf@baffier.fr>
2021-05-20 22:10:15 +0900

    Update magic_square with error function instead of predicate

--- src/magic_square.jl
+++ src/magic_square.jl
@@ -2,17 +2,16 @@ function magic_square(n, ::Val{:JuMP})
     N = n^2
     model = JuMP.Model(CBLS.Optimizer)
     magic_constant = n * (N + 1) / 2
-    Σeq = x -> sum(x) == magic_constant
 
     @variable(model, 1 ≤ X[1:n, 1:n] ≤ N, Int)
     @constraint(model, vec(X) in AllDifferent())
 
     for i in 1:n
-        @constraint(model, X[i,:] in Predicate(Σeq))
-        @constraint(model, X[:,i] in Predicate(Σeq))
+        @constraint(model, X[i,:] in SumEqualParam(magic_constant))
+        @constraint(model, X[:,i] in SumEqualParam(magic_constant))
     end
-    @constraint(model, [X[i,i] for i in 1:n] in Predicate(Σeq))
-    @constraint(model, [X[i,n + 1 - i] for i in 1:n] in Predicate(Σeq))
+    @constraint(model, [X[i,i] for i in 1:n] in SumEqualParam(magic_constant))
+    @constraint(model, [X[i,n + 1 - i] for i in 1:n] in SumEqualParam(magic_constant))
 
     return model, X
 end
